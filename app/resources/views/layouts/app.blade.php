122d8c3a20e94e812fae3688a5f997adc252a677
Michael Ozoudeh <jesmikky@gmail.com>
2021-09-26 04:42:47 +0100

    updates

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -64,7 +64,19 @@
       <script src="{{asset('/asset/example-toastr.js?ver=2.2.0')}}"></script>
     @yield('scripts')
 <!--Start of Tawk.to Script-->
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/614fe9f225797d7a8900d7dd/1fgg2jnp8';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <!--End of Tawk.to Script-->
 </body>
 
