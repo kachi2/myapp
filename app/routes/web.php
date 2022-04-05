66ed22570f90a43251f8726e554796c9a8d8b980
Michael Ozoudeh <jesmikky@gmail.com>
2022-04-05 04:15:41 +0100

    updates

--- app/routes/web.php
+++ app/routes/web.php
@@ -14,7 +14,7 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::domain('app.zenithcapital.local')->prefix('user')->group(function(){
+Route::domain('app.zenithcapital.cc')->prefix('user')->group(function(){
     Route::get('/', 'MbHomeController@Index')->name('mb.index');
     });
 
