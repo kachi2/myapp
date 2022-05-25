cf676a31250d86268eaea6f85711eb1cd914ca75
Michael Kachi <jesmikky@gmail.com>
2022-05-25 03:07:35 +0100

    updates

--- app/resources/views/auth/register.blade.php
+++ app/resources/views/auth/register.blade.php
@@ -188,16 +188,18 @@
         <script src="{{asset('/mobile/js/contact-form-script.js')}}"></script>
         <!-- main js -->
         <script src="{{asset('/mobile/js/script.js')}}"></script>
-        <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6286cead7b967b1179904c76/1g3f9em5k';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-            </script>
+      <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/628d8ee4b0d10b6f3e73e28b/1g3sfcblt';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
     </body>
 </html>
--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -86,5 +86,19 @@ s0.parentNode.insertBefore(s1,s0);
  <script src='//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'></script>
 
 <!--End of Tawk.to Script--></body>
-
+<!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/628d8ee4b0d10b6f3e73e28b/1g3sfcblt';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
+<!--End of Tawk.to Script-->
 </html>
